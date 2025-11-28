9353aa575b5b0a75212646358859b5e87313fa37
João Bosco <107318479+jbosc0@users.noreply.github.com>
2025-11-27 22:38:42 -0300

    feat: adicionar dispositivo Sound com operações on/off/setCD/setRadio/setVolume

---
+++ Sound.java
@@ -0,0 +1,7 @@
+public class Sound {
+    public void on() { System.out.println("Som ligado."); }
+    public void off() { System.out.println("Som desligado."); }
+    public void setCD() { System.out.println("CD inserido."); }
+    public void setRadio() { System.out.println("Rádio ligado."); }
+    public void setVolume() { System.out.println("Volume ajustado."); }
+}
