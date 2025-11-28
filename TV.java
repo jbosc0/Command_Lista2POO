dda8a5a50e63918190a3a4eb7e779b36cfcef098
João Bosco <107318479+jbosc0@users.noreply.github.com>
2025-11-27 22:32:01 -0300

    feat: adicionar dispositivo TV com operações on/off/setChannel/setVolume

---
+++ TV.java
@@ -0,0 +1,17 @@
+public class TV {
+    public void on() {
+        System.out.println("TV ligada.");
+    }
+
+    public void off() {
+        System.out.println("TV desligada.");
+    }
+
+    public void setChannel() {
+        System.out.println("Canal ajustado.");
+    }
+
+    public void setVolume() {
+        System.out.println("Volume ajustado.");
+    }
+}
