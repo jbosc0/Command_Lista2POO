bf0704b3da9031b2e0028141a43c116fb24cc077
João Bosco <107318479+jbosc0@users.noreply.github.com>
2025-11-27 22:34:11 -0300

    feat: adicionar dispositivo GarageDoor com operações up/down/stop/lightOn/lightOff

---
+++ GarageDoor.java
@@ -0,0 +1,7 @@
+public class GarageDoor {
+    public void up() { System.out.println("Porta da garagem subiu."); }
+    public void down() { System.out.println("Porta da garagem desceu."); }
+    public void stop() { System.out.println("Porta da garagem parada."); }
+    public void lightOn() { System.out.println("Luz da garagem ligada."); }
+    public void lightOff() { System.out.println("Luz da garagem desligada."); }
+}
