283c84f7a9d11f3b6a941fdadb0b24ff7c6db032
João Bosco <107318479+jbosc0@users.noreply.github.com>
2025-11-27 22:36:12 -0300

    feat: adicionar dispositivo HotTub com operações on/off/setTemperature

---
+++ HotTub.java
@@ -0,0 +1,7 @@
+public class HotTub {
+    public void on() { System.out.println("HotTub ligado."); }
+    public void off() { System.out.println("HotTub desligado."); }
+    public void setTemperature() {
+        System.out.println("Temperatura ajustada.");
+    }
+}
