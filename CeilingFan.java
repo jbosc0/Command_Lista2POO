428ef37a35969051712bc7417f8ed82aa33ccad1
João Bosco <107318479+jbosc0@users.noreply.github.com>
2025-11-27 22:33:00 -0300

    feat: adicionar dispositivo CeilingFan com operações high/on/off/setSpeed

---
+++ CeilingFan.java
@@ -0,0 +1,6 @@
+public class CeilingFan {
+    public void high() { System.out.println("Ventilador no HIGH."); }
+    public void on() { System.out.println("Ventilador ligado."); }
+    public void off() { System.out.println("Ventilador desligado."); }
+    public void setSpeed() { System.out.println("Velocidade ajustada."); }
+}
